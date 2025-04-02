987f758939b1a88e4f58474d896ec10e083e506e
Ndunge-Makau <winniemakau@outloo.com>
2025-04-02 13:41:36 +0300

    Add Unsubscribing

--- store/config/routes.rb
+++ store/config/routes.rb
@@ -20,4 +20,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "products#index"
+
+  # Define a resource route for unsubcribe
+  resources :unsubscribe, only: [ :show ]
 end
